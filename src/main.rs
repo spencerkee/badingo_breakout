8eee5bbc5fa44211c559de99f4787006fb7fc1b3
Spencer Kee <spencerkee@gmail.com>
2022-07-05 02:01:23 -0400

    Add background color and paddle/collider components

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,24 @@ use bevy::{
     sprite::collide_aabb::{collide, Collision},
 };
 
+const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_system(bevy::input::system::exit_on_esc_system)
         .run();
+}
+
+// A paddle component
+struct Paddle {
+    speed: f32,
+}
+
+// Enum with 3 fields, contains types of collisions for the ball
+enum Collider {
+    Solid,
+    Scorable,
+    Paddle,
 }
