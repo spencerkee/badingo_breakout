cb2deb9df9f61eff71f5931b27f0418cbbb60736
Spencer Kee <spencerkee@gmail.com>
2022-07-06 21:42:57 -0400

    Add paddle movement system

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,46 @@ use bevy::{
     sprite::collide_aabb::{collide, Collision},
 };
 
+// Defines the amount of time that should elapse between each physics step.
+const TIME_STEP: f32 = 1.0 / 60.0;
+
+// Paddle size/movement
+// These constants are defined in `Transform` units.
+// Using the default 2D camera they correspond 1:1 with screen pixels.
+// The `const_vec3!` macros are needed as functions that operate on floats cannot be constant in Rust.
+const PADDLE_SIZE: Vec3 = const_vec3!([120.0, 20.0, 0.0]);
+const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
+const PADDLE_SPEED: f32 = 500.0;
+// How close can the paddle get to the wall
+const PADDLE_PADDING: f32 = 10.0;
+
+// Walls
+const WALL_THICKNESS: f32 = 10.0;
+// x coordinates
+const LEFT_WALL: f32 = -450.;
+const RIGHT_WALL: f32 = 450.;
+// y coordinates
+const BOTTOM_WALL: f32 = -300.;
+const TOP_WALL: f32 = 300.;
+
+// Colors
+const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_system(bevy::input::system::exit_on_esc_system)
         .add_startup_system(setup)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
+                .with_system(move_paddle)
+        )
         .run();
 }
-
-// Transforms
-const PADDLE_SIZE: Vec3 = const_vec3!([120.0, 20.0, 0.0]);
-
-// Colors
-const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
-const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
-
-const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
-
-// y coordinates
-const BOTTOM_WALL: f32 = -300.;
 
 // A paddle component
 #[derive(Component)]
@@ -50,8 +70,9 @@ fn setup(
     // Next we spawn a sprite to represent our paddle. A sprite is usually a simple little graphical 2D object, like a tree or a player character (like 2D Mario for example).
     let paddle_y = BOTTOM_WALL + GAP_BETWEEN_PADDLE_AND_FLOOR;
 
+    // Here we're inserting a Paddle component, a SpriteBundle, and a Collider. This seems odd, why wouldn't these be grouped together into one object? Ah I see, the spawn() creates the empty entity and we're adding components. Probably still should use a bundle.
     commands
-        .spawn()
+        .spawn() // Create an empty Entity and return an EntityCommands builder for it
         .insert(Paddle)
         .insert_bundle(SpriteBundle {
             transform: Transform {
@@ -66,4 +87,30 @@ fn setup(
             ..default()
         })
         .insert(Collider);
+}
+
+fn move_paddle(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Paddle>>,
+) {
+    let mut paddle_transform = query.single_mut();
+    let mut direction = 0.0;
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        direction -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        direction += 1.0;
+    }
+
+    // Calculate the new horizontal paddle position based on player input
+    let new_paddle_position = paddle_transform.translation.x + direction * PADDLE_SPEED * TIME_STEP;
+
+    // Update the paddle position,
+    // making sure it doesn't cause the paddle to leave the arena
+    let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 + PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
+    let right_bound = RIGHT_WALL - WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 - PADDLE_PADDING;
+
+    paddle_transform.translation.x = new_paddle_position.clamp(left_bound, right_bound);
 }
